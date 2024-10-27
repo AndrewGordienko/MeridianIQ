69ec6f4ec521ca9398da24a9f1fe475d3b11004d
Andrew <60705784+AndrewGordienko@users.noreply.github.com>
2024-10-26 23:32:39 -0400

    Add files via upload

---
+++ llama-cleanup/example.py
@@ -0,0 +1,11 @@
+from llama_cleanup.main import AddressLookup
+
+address_lookup = AddressLookup(
+    canadian_postal_codes_path="CanadianPostalCodes202403.csv",
+    us_zip_codes_path="USZIPCodes202409.csv",
+    llama_model="llama3.1"
+)
+
+# Test the lookup method
+result = address_lookup.lookup("bridge street and church street camp robinson on l6m 4x2")
+print(result)
